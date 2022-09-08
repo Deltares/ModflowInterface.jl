ac568cb8bf68a998f0fa40280cdadc425c39747e
Martijn Visser <mgvisser@gmail.com>
2022-09-08 21:48:15 +0200

    add CI and Aqua
    CI file generated with PkgTemplates, where CI is off by default.
    
    ```
     t = Template(;plugins=[GitHubActions(;windows=true,osx=true,coverage=false)])
     ```

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using Test
 using ModflowInterface
 import ModflowInterface as MF
 import BasicModelInterface as BMI
+import Aqua
 
 @testset "ModflowInterface" begin
     include("mf6lake.jl")
+    Aqua.test_all(ModflowInterface)
 end
